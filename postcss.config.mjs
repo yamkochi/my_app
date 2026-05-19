ad835906275f8fe48514b944f30c4cfd162b7bcf
yamkochi <yamkochi@gmail.com>
2026-05-19 17:46:07 +0530

    testing  config change connection

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -4,16 +4,9 @@
 //     autoprefixer: {},
 //   },
 // }
-
-// module.exports = {
-//   plugins: {
-//     "@tailwindcss/postcss": {},
-//     autoprefixer: {},
-//   },
-// }
+// postcss.config.mjs
 export default {
   plugins: {
     "@tailwindcss/postcss": {},
-    autoprefixer: {},
   },
 }
