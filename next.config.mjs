4da1e5cde341dc2492846f6842d44641f84aa939
yamkochi <yamkochi@gmail.com>
2026-05-22 12:59:08 +0530

    yamkochi.com/bridges rmoved -1

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,5 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   /* config options here */
-//   reactCompiler: true,
-// };
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/bridges",
-  assetPrefix: "/bridges",
   reactCompiler: true,
   reactStrictMode: true,
   // Ensures Leaflet assets don't throw unexpected token errors during compilation
